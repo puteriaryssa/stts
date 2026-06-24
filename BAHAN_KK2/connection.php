4fc6ce70caa553e22c9a649477366f51d55ac35a
puteriaryssa <m-10528988@moe-dl.edu.my>
2026-06-24 09:31:44 +0800

    Add files via upload

---
+++ BAHAN_KK2/connection.php
@@ -0,0 +1,12 @@
+<?PHP
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "stts";
+
+$conn = mysqli_connect ($servername, $username, $password, $db) or die("Unable to connect");
+
+//echo "Great Work!!";
+
+?>
